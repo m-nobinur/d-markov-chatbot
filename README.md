70c5913a96f085207a42b7137471a1a03b7b6fd7
Nobin <nobinkhan.me@gmail.com>
2022-02-21 20:12:00 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# d-markov-chatbot 🪄
+
+A discord bot that generates and sends messages based on Markov Chain Model.
---
+++ chat-bot.py
@@ -0,0 +1,9 @@
+"""
+Name: d-markov-chatbot
+Description: A discord bot that generates and sends messages based on Markov Chain Model.
+Author: Mohammad Nobinur
+Using https://github.com/DisnakeDev/disnake
+API: http://disnake.readthedocs.io/en/latest/api.html
+Copyright © m-nobinur 2022 - https://github.com/m-nobinur
+
+"""
